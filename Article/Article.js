afe73555ea9cf447ad5debb392e881ab2f0c52e0
ghungpham <pham.ghungj@gmail.com>
2019-04-10 14:54:49 -0400

    stretch: close

--- Article/Article.js
+++ Article/Article.js
@@ -6,11 +6,15 @@ class Article {
     this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
+    
   
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', () => this.expandArticle())
+    this.expandButton.addEventListener('click', () => this.expandArticle());
+
+    this.close = this.domElement.querySelector('.close');
+    this.close.addEventListener('click', () => this.closeArticle());
     
   }
 
@@ -18,6 +22,10 @@ class Article {
     // Using our reference to the domElement, toggle a class to expand or hide the article
     this.domElement.classList.toggle('article-open');
 
+  }
+
+  closeArticle() {
+    this.domElement.classList.toggle('article-close')
   }
 }
 
